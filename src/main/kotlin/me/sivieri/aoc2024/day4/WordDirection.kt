1efd8e2b91f5dba78079e008a9c6bbecfb1bbda2
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2024-12-04 17:43:09 +0100

    feat: day 4 part 1

--- src/main/kotlin/me/sivieri/aoc2024/day4/WordDirection.kt
+++ src/main/kotlin/me/sivieri/aoc2024/day4/WordDirection.kt
@@ -1,12 +1,14 @@
 package me.sivieri.aoc2024.day4
 
-enum class WordDirection(x: Int, y: Int) {
-    UP(0, -1),
-    UP_RIGHT(1, -1),
-    RIGHT(1, 0),
-    DOWN_RIGHT(1, 1),
-    DOWN(0, 1),
-    DOWN_LEFT(-1, 1),
-    LEFT(-1, 0),
-    UP_LEFT(-1, -1);
+import me.sivieri.aoc2024.common.Coordinate2D
+
+enum class WordDirection(val c: Coordinate2D) {
+    UP(Coordinate2D(0, -1)),
+    UP_RIGHT(Coordinate2D(1, -1)),
+    RIGHT(Coordinate2D(1, 0)),
+    DOWN_RIGHT(Coordinate2D(1, 1)),
+    DOWN(Coordinate2D(0, 1)),
+    DOWN_LEFT(Coordinate2D(-1, 1)),
+    LEFT(Coordinate2D(-1, 0)),
+    UP_LEFT(Coordinate2D(-1, -1));
 }
--- src/main/kotlin/me/sivieri/aoc2024/day4/WordField.kt
+++ src/main/kotlin/me/sivieri/aoc2024/day4/WordField.kt
@@ -1,12 +1,15 @@
 package me.sivieri.aoc2024.day4
 
+import me.sivieri.aoc2024.common.Coordinate2D
+
 class WordField(
     text: String
 ) {
 
     private val letters = text
         .split("\n")
-        .map { it.toCharArray().toTypedArray() }
+        .filter { it.isNotBlank() }
+        .map { it.trim('\n').toCharArray().toTypedArray() }
         .toTypedArray()
     private val maxX = letters[0].size
     private val maxY = letters.size
@@ -14,23 +17,44 @@ class WordField(
     fun countXmasOccurrences(): Int {
         var counter = 0
 
-        (0 until maxX).forEach { x ->
-            (0 until maxY).forEach { y ->
-                if (letters[y][x] == XMAS[0]) {
-                   counter += searchXmas(x, y)
-                }
+        (0 until maxY).forEach { y ->
+            (0 until maxX).forEach { x ->
+                counter += searchXmas(Coordinate2D(x, y))
             }
         }
 
         return counter
     }
 
-    private fun searchXmas(x: Int, y: Int): Int {
-        TODO("Not yet implemented")
+    private fun searchXmas(c: Coordinate2D): Int {
+        // X
+        if (letters[c.y][c.x] != 'X') return 0
+
+        // M
+        val directionOfWord = WordDirection.values().filter { dir ->
+            val updated = updateSafe(c, dir.c)
+            updated != null && letters[updated.y][updated.x] == 'M'
+        }
+        return directionOfWord.count { dir ->
+            val locationOfM = updateSafe(c, dir.c)!!
+
+            // A
+            val locationOfA = updateSafe(locationOfM, dir.c)
+            if (locationOfA == null || letters[locationOfA.y][locationOfA.x] != 'A') return@count false
+
+            // S
+            val locationOfS = updateSafe(locationOfA, dir.c)
+            if (locationOfS == null || letters[locationOfS.y][locationOfS.x] != 'S') return@count false
+
+            return@count true
+        }
     }
 
-    companion object {
-        private const val XMAS = "XMAS"
-    }
+    private fun updateSafe(c: Coordinate2D, incr: Coordinate2D): Coordinate2D? =
+        if (c.x + incr.x >= maxX ||
+            c.x + incr.x < 0 ||
+            c.y + incr.y >= maxY ||
+            c.y + incr.y < 0) null
+        else Coordinate2D(c.x + incr.x, c.y + incr.y)
 
 }
